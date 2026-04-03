8dd1f75f029d1511cc96d0acbe6c140713ca89ab
coordinatescms-ai <coordinatescms@gmail.com>
2026-04-03 10:37:53 +0300

    Add files via upload

---
+++ config/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'smtp.mailtrap.io', // Замініть на реальний SMTP сервер
+    'port' => 2525,
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'encryption' => 'tls',
+    'from_email' => 'no-reply@mysite.test',
+    'from_name' => 'MySite Store',
+];
